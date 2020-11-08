92a02199e54daddf9b7aa39514d246d8052db6d6
Ismael Boro <ismaelboro@Ismaels-MBP.home>
2020-11-08 14:54:56 -0500

    firebase authentication with react

--- src/contexts/AuthContext.js
+++ src/contexts/AuthContext.js
@@ -1,8 +1,8 @@
 import React, { useContext, useState, useEffect } from 'react'
 import { auth } from '../firebase'
 
-const AuthContext = React.createContext()
 
+const AuthContext = React.createContext()
 export function useAuth() {
     return useContext(AuthContext)
 }
